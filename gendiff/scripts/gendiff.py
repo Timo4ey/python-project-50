66439c072d83ce991c4d9472294f0126c5730582
Timofey <tima_yakovishin@mail.ru>
2023-03-02 17:59:57 +0300

    added new function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,25 @@
 import argparse
+from gendiff.generate_diff import generate_diff
 
 
-def main():
+def command():
     description = 'Compares two configuration files and shows a difference.'
     parser = argparse.ArgumentParser(
         prog='gendiff',
         description=description)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', metavar='FORMAT', help="set format of output")
+    parser.add_argument('-f', '--format', metavar='FORMAT',
+                        help="set format of output")
 
     args = parser.parse_args()
-    print(args.first_file, args.second_file)
+    return args.first_file, args.second_file
+
+
+def main():
+    file1, file2 = command()
+    diff = generate_diff(file1, file2)
+    print(diff)
 
 
 if __name__ == '__main__':
