60e9031536d878a0dd3ac6dae97f938bb299a8dc
Timofey <tima_yakovishin@mail.ru>
2023-03-07 17:01:41 +0300

    add inner def to generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,4 @@
-from gendiff.prepare_data.prepare_data import (find_files,
-                                               prepare_data,
+from gendiff.prepare_data.prepare_data import (prepare_data,
                                                serialize_output
                                                )
 
@@ -10,27 +9,21 @@ def get_value_from_two_dicts(key, first_file, second_file):
 
 def generate_diff(file_path1, file_path2) -> str:
     first_file, second_file, keys = prepare_data(file_path1, file_path2)
-    output = '{'
     frame = '\n  {} {}: {}'
-    for k in keys:
-        key, key2 = get_value_from_two_dicts(k, first_file, second_file)
-        if first_file.get(k) == second_file.get(k):
-            output += frame.format(' ', k, key)
-        else:
-            output += frame.format('-', k, key) if \
-                (key is not type) else ''
-            output += frame.format('+', k, key2) if \
-                (key2 is not type) else ''
-    output += '\n}'
+    if not first_file and not second_file:
+        return f'{first_file}'
 
-    if len(output) <= 3:
-        return str(first_file)
-    output = serialize_output(output)
-    return output
-
-
-if __name__ == '__main__':
-
-    file1, file2 = find_files('../files/')
-    diff = generate_diff(file1, file2)
-    print(diff)
+    def dif(first_dict, second_dict, key):
+        output = ""
+        for k in key:
+            key, key2 = get_value_from_two_dicts(k, first_dict, second_dict)
+            if first_dict.get(k) == second_dict.get(k):
+                output += frame.format(' ', k, key)
+            else:
+                output += frame.format('-', k, key) if \
+                    (key is not type) else ''
+                output += frame.format('+', k, key2) if \
+                    (key2 is not type) else ''
+        output = serialize_output(output)
+        return output
+    return f'{"{"}{dif(first_file, second_file, keys)}\n{"}"}'
