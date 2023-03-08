4ece4d0eeb0bdc88c3abcb7243939121560962f7
Timofey <tima_yakovishin@mail.ru>
2023-03-08 17:43:14 +0300

    join files for tests in several files. Next step do it through fixtures

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,6 +3,34 @@ from gendiff.generate_diff import get_value_from_two_dicts, generate_diff
 from gendiff.scripts.gendiff import command
 import os
 import subprocess
+import yaml
+import json
+def fixture_path(file_path):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    path_to_file = os.path.join(current_dir + '/fixtures/' + file_path)
+    return path_to_file
+
+
+def files_loader(path):
+    the_file = fixture_path(path)
+    files = open(the_file, 'r')
+    list_if_files = files.read().rstrip().split('\n\n\n\n')
+    return list_if_files
+
+
+def json_loader(array):
+    output = []
+    for jsn in array:
+        output.append(json.loads(jsn))
+    return output
+
+
+def yaml_loader(array):
+    output = []
+    for yml in array:
+        output.append(yaml.load(yml, yaml.Loader))
+    return output
+
 
 
 def test_get_value_from_two_dicts():
@@ -13,87 +41,17 @@ def test_get_value_from_two_dicts():
     assert get_value_from_two_dicts('a', {'a': None}, {'a': True}) == (None, True)
 
 
-def test_generate_diff_json():
-    # test 1
-    directory = os.path.abspath('tests/fixtures/json_tests/test1_plain_json.txt')
-    file1 = os.path.abspath('tests/fixtures/json_tests/test_1_file1.json')
-    file2 = os.path.abspath('tests/fixtures/json_tests/test_1_file2.json')
-    with open(directory, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
-    # test 2
-    directory1 = os.path.abspath('tests/fixtures/json_tests/test2_plain_json.txt')
-    file3 = os.path.abspath('tests/fixtures/json_tests/test_2_file1.json')
-    file4 = os.path.abspath('tests/fixtures/json_tests/test_2_file2.json')
-    with open(directory1, 'r') as f:
-        assert generate_diff(file3, file4) == f.read()
-    # test 3
-    directory2 = os.path.abspath('tests/fixtures/json_tests/test3_positive_file5.txt')
-    file3 = os.path.abspath('tests/fixtures/json_tests/test_3_positive.json')
-    file4 = os.path.abspath('tests/fixtures/json_tests/test_3_positive.json')
-    with open(directory2, 'r') as f:
-        assert generate_diff(file3, file4) == f.read()
-    # test 5
-    directory3 = os.path.abspath('tests/fixtures/json_tests/test4_empty_full.txt')
-    file1 = os.path.abspath('tests/fixtures/json_tests/test_4_empty_file1.json')
-    file2 = os.path.abspath('tests/fixtures/json_tests/test_4_file2.json')
-    with open(directory3, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
-    # test 6
-    directory4 = os.path.abspath('tests/fixtures/json_tests/test4_full_empty.txt')
-    file1 = os.path.abspath('tests/fixtures/json_tests/test_4_file2.json')
-    file2 = os.path.abspath('tests/fixtures/json_tests/test_4_empty_file1.json')
-    with open(directory4, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
-    # test 6
-    directory5 = os.path.abspath('tests/fixtures/json_tests/test7_empty_empty.txt')
-    file1 = os.path.abspath('tests/fixtures/json_tests/test_4_empty_file1.json')
-    file2 = os.path.abspath('tests/fixtures/json_tests/test_4_empty_file1.json')
-    with open(directory5, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
+def test_json():
+    jsn_files = json_loader(files_loader('jsons.txt'))
+    answers = files_loader('answers.txt')
+    for i, v in zip(range(0, len(jsn_files) - 1, 2), answers):
+        assert generate_diff(jsn_files[i], jsn_files[i + 1]) == v
 
 
-def test_generate_diff_yml():
-    directory = os.path.abspath('tests/fixtures/json_tests/test1_plain_json.txt')
-    file1 = os.path.abspath('tests/fixtures/yml_tests/test_1_yaml_file1.yml')
-    file2 = os.path.abspath('tests/fixtures/yml_tests/test_1_yaml_file2.yml')
-    with open(directory, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
-    directory1 = os.path.abspath('tests/fixtures/json_tests/test2_plain_json.txt')
-    file3 = os.path.abspath('tests/fixtures/yml_tests/test_2_yaml_file1.yml')
-    file4 = os.path.abspath('tests/fixtures/yml_tests/test_2_yaml_file2.yml')
-    with open(directory1, 'r') as f:
-        assert generate_diff(file3, file4) == f.read()
-    # test 3
-    directory2 = os.path.abspath('tests/fixtures/json_tests/test3_positive_file5.txt')
-    file3 = os.path.abspath('tests/fixtures/yml_tests/test_3_yaml_file1.yml')
-    file4 = os.path.abspath('tests/fixtures/yml_tests/test_3_yaml_file2.yml')
-    with open(directory2, 'r') as f:
-        assert generate_diff(file3, file4) == f.read()
-    # test 5
-    directory3 = os.path.abspath('tests/fixtures/json_tests/test4_empty_full.txt')
-    file1 = os.path.abspath('tests/fixtures/yml_tests/test_4_yaml_file1.yml')
-    file2 = os.path.abspath('tests/fixtures/yml_tests/test_4_yaml_file2.yml')
-    with open(directory3, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
-    # test 6
-    directory4 = os.path.abspath('tests/fixtures/json_tests/test4_full_empty.txt')
-    file1 = os.path.abspath('tests/fixtures/yml_tests/test_4_yaml_file2.yml')
-    file2 = os.path.abspath('tests/fixtures/yml_tests/test_4_yaml_file1.yml')
-    with open(directory4, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
-    # test 6
-    directory5 = os.path.abspath('tests/fixtures/json_tests/test7_empty_empty.txt')
-    file1 = os.path.abspath('tests/fixtures/yml_tests/test_4_yaml_file1.yml')
-    file2 = os.path.abspath('tests/fixtures/yml_tests/test_4_yaml_file1.yml')
-    with open(directory5, 'r') as f:
-        assert generate_diff(file1, file2) == f.read()
-
-
-def test_command():
-    execute = subprocess.getoutput("poetry run gendiff -f plain test_1_file1.json test_1_file2.json")
-    directory = os.path.abspath('tests/fixtures/json_tests/test1_plain_json.txt')
-    with open(directory, 'r') as f:
-        assert execute == f.read()
-
+def test_yaml():
+    jsn_files = yaml_loader(files_loader('yamls.txt'))
+    answers = files_loader('answers.txt')
+    for i, v in zip(range(0, len(jsn_files) - 1, 2), answers):
+        assert generate_diff(jsn_files[i], jsn_files[i + 1]) == v
 
 
