f40ee9a3e3692b3cd4c2c4674105ca3f7faf6305
Timofey <tima_yakovishin@mail.ru>
2023-03-14 14:06:27 +0300

    added if statement

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 import argparse
 from gendiff.generate_diff import generate_diff
 from gendiff.prepare_data.prepare_data import prepare_data
+from gendiff.scripts.stylish.stylish import stylish
 
 
 def command():
@@ -11,19 +12,20 @@ def command():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', metavar='FORMAT',
-                        help="set format of output", default="plain")
+                        help="set format of output", default="stylish")
 
     args = parser.parse_args()
-    return args.first_file, args.second_file
+    return args.first_file, args.second_file, args.format
 
 
 def main():
-    file1, file2 = command()
-    # path1, path2 = find_files(file1, file2)
+    file1, file2, style = command()
     first_data, second_data = prepare_data(file1, file2)
-
-    diff = generate_diff(first_data, second_data)
-    print(diff)
+    if style == "stylish":
+        output = stylish(first_data, second_data)
+    else:
+        output = generate_diff(first_data, second_data)
+    print(output)
 
 
 if __name__ == '__main__':
