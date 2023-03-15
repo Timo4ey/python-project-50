99708606dadbc962ee57415f7b7472e9a162291f
Timofey <tima_yakovishin@mail.ru>
2023-03-15 17:30:38 +0300

    fix7 added function in init

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,5 +1,5 @@
-from gendiff.gen_diff_simple import dif
+from gendiff.scripts.stylish.stylish import stylish
 
 
 def generate_diff(dictionary_1: dict, dictionary_2: dict) -> str:
-    return dif(dictionary_1, dictionary_2).strip('\n')
+    return stylish(dictionary_1, dictionary_2).strip('\n')
