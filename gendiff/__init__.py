9143adfd469f451f378a1c34dd5c3169f8269520
Timofey <tima_yakovishin@mail.ru>
2023-03-15 17:34:08 +0300

    fix8 added function in init

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,5 +1,8 @@
+from gendiff.prepare_data.prepare_data import prepare_data
 from gendiff.scripts.stylish.stylish import stylish
 
 
 def generate_diff(dictionary_1: dict, dictionary_2: dict) -> str:
-    return stylish(dictionary_1, dictionary_2).strip('\n')
+    file1 = prepare_data(dictionary_1, dictionary_2)
+    file2 = prepare_data(dictionary_1, dictionary_2)
+    return stylish(file1, file2).strip('\n')
