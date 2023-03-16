d7ec147f60a73e0d65f694842e96cdf560e71814
Timofey <tima_yakovishin@mail.ru>
2023-03-16 08:02:08 +0300

    [#fix14] chanched value space

--- gendiff/scripts/formartter/formartter.py
+++ gendiff/scripts/formartter/formartter.py
@@ -1,6 +1,6 @@
 def convert_to_format(space_filler: str, symbol: str, key: str | int,
                       value) -> list[str]:
     if isinstance(value, str):
-        value = f'{value}' if len(value) < 0 else value
+        value = f'{value}' if len(value) == 0 else value
     return [''.join([space_filler, f' {symbol}', f' {key}:', f' {value}']
                     ).rstrip(' ')]
